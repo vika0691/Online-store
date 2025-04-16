650bd46a1853eef2e5e9db7b222e0f23af499eaf
Vika <victoriafedorova1968@gmail.com>
2025-04-16 19:32:59 +0300

    Подправила коллекции

--- ui/js/handleToProductCart.ts
+++ ui/js/handleToProductCart.ts
@@ -28,33 +28,3 @@ export function addProduct(product: string) {
     // Если товара нет в корзине, добавляем его с количеством 1
     Storage.setItem({ id, ...data, count: 1 });
 }
-
-
-
-// import { Storage, Product } from './cart.js'
-// const btn = document.getElementsByClassName("cat-card")
-
-// for (const el of Array.from(btn)) {
-//    el.addEventListener("click", (event: Event) => {
-//     const metaData = (el as HTMLElement).dataset.product?.replace('\n', '');
-
-//     if(metaData !== undefined) {
-//         addProduct(metaData)
-//     } else {
-//         console.warn('el is undefined', (el as HTMLElement).dataset)
-//     }
-//    })
-// }
-
-// export function addProduct(product: string) {
-//     const { id, ...data } = JSON.parse(product) as Product
-//     const resultGetItem = Storage.getItem(String(id))
-//     console.log(resultGetItem, product, id)
-
-//     if(resultGetItem !== undefined) {
-//         Storage.updateItem(String(id), { id, ...data })
-//         return;
-//     }
-
-//     Storage.setItem({ id, ...data, count: 1 });
-// }
